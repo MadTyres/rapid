f5a5fc7dbf4e7c6c82e9f028c669f083592c347a
Mathias Tværmose Gleerup <mtg@lunar.app>
2024-05-09 19:38:07 +0200

    adding time generators

--- time.go
+++ time.go
@@ -7,10 +7,32 @@ const nanoSecondsInSeconds = 1_000_000_000
 
 // generates a time in the range from unix 0 to a year into the future.
 func Time() *Generator[time.Time] {
-	return Custom[time.Time](func(t *T) time.Time {
+	return Custom(func(t *T) time.Time {
 		now := time.Now().Unix()
 		secGen := Int64Range(0, now+int64(secondsInYear))
 		nsecGen := Int64Range(0, int64(nanoSecondsInSeconds))
+		stamp := time.Unix(secGen.Draw(t, ""), int64(nsecGen.Draw(t, "")))
+
+		//todo implement drawing a timezone.
+		return stamp
+	})
+}
+
+func TimeTo(to time.Time) *Generator[time.Time] {
+	return Custom(func(t *T) time.Time {
+		secGen := Int64Range(0, to.Unix())
+		nsecGen := Int64Range(0, int64(1000))
+		stamp := time.Unix(secGen.Draw(t, ""), int64(nsecGen.Draw(t, "")))
+
+		//todo implement drawing a timezone.
+		return stamp
+	})
+}
+
+func TimeInterval(from, to time.Time) *Generator[time.Time] {
+	return Custom(func(t *T) time.Time {
+		secGen := Int64Range(from.Unix(), to.Unix())
+		nsecGen := Int64Range(0, int64(1000))
 		stamp := time.Unix(secGen.Draw(t, ""), int64(nsecGen.Draw(t, "")))
 
 		//todo implement drawing a timezone.
